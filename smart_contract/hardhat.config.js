5323f7de5e0ee6286d8405f00211c10be3e982cf
Vinit Gupta <vinitman35@gmail.com>
2022-11-14 12:34:33 +0530

    deployed the smart contract

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/eLAumq6hOOGcUIGd-cjiWxjemj_DeDhc
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle")
+
 module.exports = {
-  solidity: "0.8.17",
-};
+  solidity : '0.8.0',
+  networks : {
+    goerli : {
+      url : 'https://eth-goerli.g.alchemy.com/v2/eLAumq6hOOGcUIGd-cjiWxjemj_DeDhc',
+    accounts : ['46a99e8c072294e4dc017acbb1968759f2858e25cb6872e7a257590fd5390236']
+  }
+  }
+}
