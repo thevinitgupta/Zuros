1c96fc1723e8724e385160571e3c1c8294eb94fb
Vinit Gupta <vinitman35@gmail.com>
2022-12-26 19:52:25 +0530

    add connect to wallet and send ethers functionality

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -2,7 +2,10 @@ import  {AiFillPlayCircle} from 'react-icons/ai'
 import  {SiEthereum} from 'react-icons/si'
 import  {BsInfoCircle} from 'react-icons/bs'
 
+import {TransactionContext} from "../context/TransactionContext"
+
 import  {Loader} from "./"
+import { useContext } from 'react'
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -12,18 +15,26 @@ const Input = ({placeholder, name,type,value,handleChange}) =>(
     step="0.0001"
     value={value}
     onChange={(e)=>{
-        handleChange(e);
+        handleChange(e, name);
     }}
     className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
     />
 )
 
 const Welcome = () =>{
-    const connectWallet = () =>{}
+    const {connectWallet, currentAccount, formData, handleChange, sendTransaction } = useContext(TransactionContext);
 
-    const handleSubmit = () =>{
+    const handleSubmit = (e) =>{
+        const {addressTo, amount, keyword, message} = formData;
+
+        e.preventDefault();
         
+        if(!addressTo || !amount || !keyword || !message) return;
+
+        sendTransaction();
+
     }
+
     return(
         <div className="flex w-full justify-center items-center">
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -34,7 +45,7 @@ const Welcome = () =>{
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
             Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
           </p>
-            <button
+            {!currentAccount && <button
               type="button"
               onClick={connectWallet}
               className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
@@ -43,7 +54,7 @@ const Welcome = () =>{
               <p className="text-white text-base font-semibold">
                 Connect Wallet
               </p>
-            </button>
+            </button>}
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${commonStyles}`}>
@@ -82,14 +93,14 @@ const Welcome = () =>{
             </div>
           </div>
           <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-            <Input placeholder="Address To" name="addressTo" type="text" />
-            <Input placeholder="Amount (ETH)" name="amount" type="number"  />
-            <Input placeholder="Keyword (Gif)" name="keyword" type="text" />
-            <Input placeholder="Enter Message" name="message" type="text" />
+            <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+            <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange}  />
+            <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+            <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-            {true
+            {false
               ? <Loader />
               : (
                 <button
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x5736b5D08E8E992b19dD5E51EfCfB5Da44897dFf';
