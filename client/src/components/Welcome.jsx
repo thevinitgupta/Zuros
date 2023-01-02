c57c8d171356bcc7e230c160b93f90130691d601
Vinit Gupta <vinitman35@gmail.com>
2023-01-02 20:18:38 +0530

    add login function on button

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -23,7 +23,7 @@ const Input = ({placeholder, name,type,value,handleChange}) =>(
 )
 
 const Welcome = () =>{
-    const {connectWallet, currentAccount, formData, handleChange, sendTransaction } = useContext(TransactionContext);
+    const {connectWallet, currentAccount, formData, handleChange, sendTransaction, isLoading  } = useContext(TransactionContext);
 
     const handleSubmit = (e) =>{
         const {addressTo, amount, keyword, message} = formData;
@@ -101,7 +101,7 @@ const Welcome = () =>{
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-            {false
+            {isLoading
               ? <Loader />
               : (
                 <button
